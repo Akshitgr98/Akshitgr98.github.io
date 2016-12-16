72308c691c6ade770692a52c91fef419024430d2
Akshitgr98 <helloak.1998@gmail.com>
2016-12-16 18:14:52 +0530

    Preface

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ $name=test($_POST["Name"]);
 $email=test($_POST["Email"]);
 $message=test($_POST["Message"]);
 $mobile=$_POST["Mobilenumber"];
-$mailto="akshit.grover2016@gmail";
+$mailto="akshit.grover2016@gmail.com";
 $from="do_not_reply@akshitgrover.website";
 function test($data) {
   $data = trim($data);
@@ -13,5 +13,6 @@ function test($data) {
 }
 $a="Name: ".$name."Mobile: ".$mobile."Message: ".$message;
 mail($mailto,"From Preface",$a,$from);
+echo "<script>alert('Your Form Has Been Submitted to '.$mailto);</script>";
 header('Location: index.html');
 ?>
